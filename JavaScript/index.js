feeb82b91f0fb4f3a3649b554d54709a9e61cd64
SDelphin <52865852+SDelphin@users.noreply.github.com>
2019-11-08 16:00:40 -0500

    Photography web

---
+++ JavaScript/index.js
@@ -0,0 +1,32 @@
+function myFunction(){
+  var topNav = document.querySelector("#topNav"); 
+  if (topNav.className==="NavigationBar") {
+    topNav.className="NavBar";
+  }
+  else{
+    topNav.className="NavigationBar";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
